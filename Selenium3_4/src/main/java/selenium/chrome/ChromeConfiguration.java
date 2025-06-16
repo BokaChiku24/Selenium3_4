be79204302d97467f2d239977cad115348226185
kunalchavan <kunal24chavan@gmail.com>
2025-06-16 22:51:58 +0530

    Chrome Configuration Selenium 3 and 4

---
+++ Selenium3_4/src/main/java/selenium/chrome/ChromeConfiguration.java
@@ -0,0 +1,18 @@
+package selenium.chrome;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeConfiguration {
+
+	public static void main(String[] args) {
+		// Selenium 3.x
+		System.setProperty("webdriver.chrome.driver", "/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/chromedriver");
+		
+		//Selenium 4.x
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+}
