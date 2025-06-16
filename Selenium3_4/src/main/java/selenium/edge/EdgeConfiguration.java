6e8d15a1b0b7bcc37e402157a1797ccdb87f1049
kunalchavan <kunal24chavan@gmail.com>
2025-06-16 23:16:28 +0530

    Edge Legacy Browser

--- Selenium3_4/src/main/java/selenium/edge/EdgeConfiguration.java
+++ Selenium3_4/src/main/java/selenium/edge/EdgeConfiguration.java
@@ -7,6 +7,14 @@ public class EdgeConfiguration {
 
 	public static void main(String[] args) {
 
+		/*
+		 * Legacy Edge 18 or less than 18:
+		 * Open the cmd command and type:
+		 * DISM.exe /Online /Add-Capablitiy /CapabilityName:Microsoft.WebDriver~~~~0.0.1.0
+		 * and hit on the enter and it install it in the system then no need to use System.setProperty()
+		 * for legacy browser
+		 */
+		
 		// Selenium 3.x
 		System.setProperty("webdriver.chrome.driver", "/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/msedgedriver");
 		
