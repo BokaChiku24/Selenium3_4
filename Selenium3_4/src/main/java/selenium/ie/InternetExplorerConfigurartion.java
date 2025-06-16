aaac99034e673503ff6cf489f8eb15df8939e352
kunalchavan <kunal24chavan@gmail.com>
2025-06-16 23:10:17 +0530

    Internet Explorer

--- Selenium3_4/src/main/java/selenium/ie/InternetExplorerConfigurartion.java
+++ Selenium3_4/src/main/java/selenium/ie/InternetExplorerConfigurartion.java
@@ -2,15 +2,20 @@ package selenium.ie;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class InternetExplorerConfigurartion {
 
 	public static void main(String[] args) {
 		// Selenium 3.x
 		System.setProperty("webdriver.ie.driver", "/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/IEDriverServer.exe");
-		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+		dc.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		InternetExplorerOptions ieo = new InternetExplorerOptions(dc);
 		//Selenium 4.x
-		WebDriver driver = new InternetExplorerDriver();
+		WebDriver driver = new InternetExplorerDriver(ieo);
 		driver.get("https://www.google.com");
 		driver.quit();
 
