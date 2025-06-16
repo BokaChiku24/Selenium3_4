8acd0305b011e515b92e6b5d984b42c293f8e806
kunalchavan <kunal24chavan@gmail.com>
2025-06-16 23:04:17 +0530

    Internet Explorer Configuration for windows.

---
+++ Selenium3_4/src/main/java/selenium/ie/InternetExplorerConfigurartion.java
@@ -0,0 +1,19 @@
+package selenium.ie;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InternetExplorerConfigurartion {
+
+	public static void main(String[] args) {
+		// Selenium 3.x
+		System.setProperty("webdriver.ie.driver", "/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/IEDriverServer.exe");
+		
+		//Selenium 4.x
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.quit();
+
+	}
+
+}
