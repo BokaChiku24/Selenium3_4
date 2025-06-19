701d0f689c5503c3d45dc9b1e11597c301a61955
kunalchavan <kunal24chavan@gmail.com>
2025-06-19 23:16:48 +0530

    Log$j Added

--- Selenium3_4/src/main/java/selenium/webDriverBasics/WebDriverbasics.java
+++ Selenium3_4/src/main/java/selenium/webDriverBasics/WebDriverbasics.java
@@ -1,5 +1,6 @@
 package selenium.webDriverBasics;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,11 +8,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverbasics {
 
+	static Logger log = Logger.getLogger(WebDriverbasics.class);
 	public static void main(String[] args) {
+		log.info("WebDriverManager Initialize the ChromeDriver");
 		WebDriverManager.chromedriver().setup();
+		log.info("WebDriverManager Initialize the ChromeDriver Successfully");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.way2automation.com/");
+		log.info("WebDriverManager Navigated To: " + "https://www.way2automation.com/");
 		driver.quit();
+		log.info("WebDriverManager Instance Quit");
 	}
 
 }
