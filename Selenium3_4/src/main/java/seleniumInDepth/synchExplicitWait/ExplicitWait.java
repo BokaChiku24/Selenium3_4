4d669630e6cdf7ecd0b5bc90dddc24226ddacd39
kunalchavan <kunal24chavan@gmail.com>
2025-06-21 01:16:04 +0530

    ExplicitWait

--- Selenium3_4/src/main/java/seleniumInDepth/synchExplicitWait/ExplicitWait.java
+++ Selenium3_4/src/main/java/seleniumInDepth/synchExplicitWait/ExplicitWait.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import seleniumInDepth.synchIssuesImplicitWait.ImpicitWait;
@@ -26,12 +28,13 @@ public class ExplicitWait {
 		driver.get("https://www.gmail.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		WebElement emailID = driver.findElement(By.id("identifierId"));
 		emailID.sendKeys("k.chavan24@gmail.com");
 		log.info("Entered EmailId is: " + emailID.getAttribute("value"));
 		driver.findElement(By.xpath("//div[@class='O1Slxf']/div[1]/div[@jsname='Njthtb']")).click();
 		log.info("Next button is clicked");
-		driver.findElement(By.xpath("//input[@name='Passwd']")).sendKeys("123123");
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//input[@name='Passwd']")))).sendKeys("123123");
 		driver.findElement(By.id("passwordNext")).click();
 		String incorrectPassowrdMessage = driver.findElement(By.xpath("(//div[@jsname='h9d3hd'])[1]")).getText();
 		Assert.assertEquals(incorrectPassowrdMessage, "Wrong password. Try again or click Forgot password to reset it.");
