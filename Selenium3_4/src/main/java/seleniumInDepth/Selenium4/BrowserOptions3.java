effb57942cca59c00eaf9ff1322d80dce47f15af
kunalchavan <kunal24chavan@gmail.com>
2025-06-26 01:25:31 +0530

    Browser Options

--- Selenium3_4/src/main/java/seleniumInDepth/Selenium4/BrowserOptions3.java
+++ Selenium3_4/src/main/java/seleniumInDepth/Selenium4/BrowserOptions3.java
@@ -1,13 +1,27 @@
 package seleniumInDepth.Selenium4;
 
-import org.openqa.selenium.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-public class BrowserOptions3{
-    public static void main(String[] args) {
-        Map<String,String> map = new Map<String,String>();
-        map.put("device","iPhone 15");
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-        ChromeOption option = new ChromeOption();
-        option.
-    }
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserOptions3 {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("deviceName", "iPhone X");
+
+		ChromeOptions option = new ChromeOptions();
+		option.setExperimentalOption("mobileEmulation", map);
+		option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver(option);
+		driver.get("https://www.selenium.dev");
+		// driver.quit();
+
+	}
 }
