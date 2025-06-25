5c136bbbd28bb27c0812ecadc8936e8c038b251b
kunalchavan <kunal24chavan@gmail.com>
2025-06-26 00:04:42 +0530

    Selenium 4.x

---
+++ Selenium3_4/src/main/java/seleniumInDepth/Selenium4/BrowserOptions.java
@@ -0,0 +1,34 @@
+package seleniumInDepth.Selenium4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BrowserOptions {
+	public static String browser = "Chrome";
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+
+		if (browser.equals("Firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					"/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/geckodriver");
+			FirefoxOptions fireOption = new FirefoxOptions();
+			fireOption.addArguments("--headless");
+			driver = new FirefoxDriver(fireOption);
+		} else if (browser.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/chromedriver");
+			ChromeOptions chromeOption = new ChromeOptions();
+			chromeOption.addArguments("--headless");
+			driver = new ChromeDriver(chromeOption);
+		}
+
+		driver.get("https://www.selenium.dev");
+		System.out.println((driver.getTitle()));
+		driver.quit();
+	}
+
+}
---
+++ Selenium3_4/src/main/java/seleniumInDepth/Selenium4/BrowserOptions2.java
@@ -0,0 +1,43 @@
+package seleniumInDepth.Selenium4;
+
+import java.util.Collections;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class BrowserOptions2 {
+	public static String browser = "Chrome";
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+
+		if (browser.equals("Firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					"/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/geckodriver");
+			FirefoxOptions fireOption = new FirefoxOptions();
+			// fireOption.addArguments("--headless");
+			fireOption.setAcceptInsecureCerts(true);
+			fireOption.addArguments("window-size=1080,1080");
+			fireOption.addArguments("incognito");
+			driver = new FirefoxDriver(fireOption);
+		} else if (browser.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"/Users/kunalchavan/git/Selenium3_4/SeleniumDrivers/chromedriver");
+			ChromeOptions chromeOption = new ChromeOptions();
+			// chromeOption.addArguments("--headless");
+			chromeOption.setAcceptInsecureCerts(true);
+			chromeOption.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+			chromeOption.addArguments("window-size=1080,1080");
+			chromeOption.addArguments("incognito");
+			driver = new ChromeDriver(chromeOption);
+		}
+
+		driver.get("https://expired.badssl.com/");
+		System.out.println((driver.getTitle()));
+		driver.close();
+	}
+
+}
