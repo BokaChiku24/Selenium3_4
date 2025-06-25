3256dbfad97bcd9d6ca365353e86b42cef9236be
kunalchavan <kunal24chavan@gmail.com>
2025-06-26 01:16:10 +0530

    BrowserOption3 Class Added

---
+++ Selenium3_4/src/main/java/seleniumInDepth/Selenium4/BrowserOptions3.java
@@ -0,0 +1,13 @@
+package seleniumInDepth.Selenium4;
+
+import org.openqa.selenium.*;
+
+public class BrowserOptions3{
+    public static void main(String[] args) {
+        Map<String,String> map = new Map<String,String>();
+        map.put("device","iPhone 15");
+
+        ChromeOption option = new ChromeOption();
+        option.
+    }
+}
